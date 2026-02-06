372743ab4274a262b67f1bcf3e79e2c46b838a32
louischarleshaynes <louisch123@yahoo.com>
2026-02-05 17:57:30 -0800

    Updated Interpreter from 3.11 to 3.10 to fix test run issue

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 def create_driver():
     options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--window-size=1920,1080")
+
+    # For local debugging, keep headless OFF
+    # For CI, turn it ON
+    # options.add_argument("--headless=new")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
-    driver.maximize_window()
     return driver
